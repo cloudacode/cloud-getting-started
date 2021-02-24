f6d803dbb6dde39a9959fb477eebab24e4918720
kcfigaro <kcfigaro@gmail.com>
2021-02-24 23:04:45 +0100

    added sample flask app

---
+++ sample-flask-app/db_config.py
@@ -0,0 +1,12 @@
+from app import app
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'cloudme'
+app.config['MYSQL_DATABASE_DB'] = 'cloud_user'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
