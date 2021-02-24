f948d89c6b580186e45ede7b4644977d48604ddc
kcfigaro <kcfigaro@gmail.com>
2021-02-24 23:07:22 +0100

    updated config

--- sample-flask-app/db_config.py
+++ sample-flask-app/db_config.py
@@ -5,7 +5,7 @@ mysql = MySQL()
  
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'cloudme'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'YOUR_DB_PASSWORD'
 app.config['MYSQL_DATABASE_DB'] = 'cloud_user'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
