eee6b8a7fe856f2a66cda8ed5b74c7b4e2ed0fad
kcfigaro <kcfigaro@gmail.com>
2021-07-05 22:47:19 +0200

    moved the codes to coolstuff

--- sample-flask-app/models.py
+++
@@ -1,9 +0,0 @@
-from flask_table import Table, Col, LinkCol
- 
-class Results(Table):
-    user_id = Col('Id')
-    user_name = Col('Name')
-    user_email = Col('Email')
-    user_password = Col('Password')
-    user_bio = Col('Bio')
-    # user_password = Col('Password', show=False)
